4768a0c02e428a144c1a6926668111a5a7ac9ebc
Dominic Tubach <tubach@systopia.de>
2026-02-16 12:17:18 +0100

    Remove rebuild-mo.sh

--- rebuild-mo.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# rebuild the translation database in machine readable format
-# run this script after each update of translation text or
-# once new messages had been added.
-
-msgfmt --statistics -o l10n/de_DE/LC_MESSAGES/newsletter.mo l10n/de_DE/LC_MESSAGES/newsletter.po
