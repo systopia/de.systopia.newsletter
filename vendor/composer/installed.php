95caa3dc0c596b31feab41d9a142bfe0a6633c36
Dominic Tubach <tubach@systopia.de>
2026-02-24 12:27:24 +0100

    Set version to 1.1.0-beta2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'systopia/de.systopia.newsletter',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f6776a53b59e003b385bcd806426e70f62d93f17',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'systopia/de.systopia.newsletter' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f6776a53b59e003b385bcd806426e70f62d93f17',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
